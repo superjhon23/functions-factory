c86e5fe99526d12f35d49770e868c63ef0013fb7
Jhonatan Moreno <jhon.moreno.dev@gmail.com>
2023-08-10 10:57:26 -0600

    Initial commit

---
+++ factory.js
@@ -0,0 +1,6 @@
+/* 
+"¡El viaje de mil millas comienza con un solo paso! 
+Cada línea de código que escribas te acerca más a la realización de tu visión.
+No temas a los desafíos, ya que cada obstáculo es una oportunidad para crecer.
+¡Hoy es el día perfecto para empezar a convertir tus ideas en realidad! 💪🚀" */
+console.log("Comenzemos el viaje!! 🚀");
